c67594b91ed022383d0e61aa5c557a95b7550447
VTU26519 RAJA <vtu26519@veltech.edu.in>
2026-02-06 10:18:04 +0530

    Time: 3 ms (10.65%), Space: 46.6 MB (22.06%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+
+        for (int x : pushed) {
+            stack.push(x);
+
+            // Pop while top matches popped sequence
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        // If stack empty, sequence is valid
+        return stack.isEmpty();
+    }
+}
