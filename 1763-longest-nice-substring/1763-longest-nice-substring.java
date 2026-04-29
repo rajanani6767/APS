56ff1fea55fcef3c66b81270f61005806e68de8c
VTU26519 RAJA <vtu26519@veltech.edu.in>
2026-04-29 15:13:44 +0530

    Time: 3 ms (60.9%), Space: 43.9 MB (56.44%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public String longestNiceSubstring(String s) {
+        return helper(s);
+    }
+
+    private String helper(String s) {
+        if (s.length() < 2) return "";
+
+        // Track characters in this substring
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(Character.toLowerCase(c)) && set.contains(Character.toUpperCase(c))) {
+                continue; // valid character
+            }
+            // Split around invalid character
+            String left = helper(s.substring(0, i));
+            String right = helper(s.substring(i + 1));
+            return left.length() >= right.length() ? left : right;
+        }
+
+        return s; // whole substring is nice
+    }
+}
