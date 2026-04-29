99617ec03a866608a408afa26d85d4e82f8a77a2
VTU26519 RAJA <vtu26519@veltech.edu.in>
2026-04-29 15:26:10 +0530

    Time: 1 ms (88.89%), Space: 44.2 MB (39.24%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+        result.add(new ArrayList<>(temp)); // add current subset
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);             // include nums[i]
+            backtrack(result, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);  // exclude nums[i]
+        }
+    }
+}
